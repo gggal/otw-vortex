e9e156c6bdaf119971603fed007491f68307f986
gggal <gggrudeva@gmail.com>
2019-12-04 22:24:00 +0200

    Adds lvl 0 solution

---
+++ vortex0/README.md
@@ -0,0 +1,3 @@
+$ python --version
+Python 3.7.4
+$ python vortex0.py
--- vortex0/vortex0.py
+++ vortex0/vortex0.py
@@ -1,17 +1,24 @@
-import socket
+from socket import *
 import struct
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# binding the socket
+s = socket(AF_INET, SOCK_STREAM)
 s.connect(('vortex.labs.overthewire.org',5842))
-total = 0
+
+# 4 values are to be read from server
+res = 0
 for x in range(0,4,1):
 	data = s.recv(4)
-	total+= struct.unpack("<I", data)[0]
 
-total = struct.pack("<I",(total & 0xFFFFFFFF))
-s.send(total)
+        # each value should be a Little-Endian unsigned int
+	res += struct.unpack("<I", data)[0]
+
+# trim the result to be a 4 byte Little-Endian unsigned int
+res = struct.pack("<I",(res & 0xFFFFFFFF))
+s.send(res)
 response = s.recv(1000)
-print("Recieved: %s" % response)
+
+print("Credentials are: %s" % response)
 
 s.close()
 
